7c2f9c4dc829fdfc66ae83db3e435864202dda44
melinasaa <629419@edu.vaniercollege.qc.ca>
2025-02-27 09:46:46 -0500

    hello

--- public_html/web2/project1/JavaScript/spa.js
+++ public_html/web2/project1/JavaScript/spa.js
@@ -1,21 +1,24 @@
 console.log('spa.js lodaded')
 
-let cards = document.querySelectorAll('#move')
+let slide = document.querySelector('#slide')
 
-for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function (event) {
-        //console.log(this.id);
-        this.classList.toggle('move-down')
-    })
+slide.addEventListener('click', function (event) {
+    //console.log(this.id);
+    this.classList.toggle('left-right')
+})
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
-
-let images = document.querySelectorAll('#slide')
-
-for (var i = 0; i < cards.length; i++) {
-    images[i].addEventListener('click', function (event) {
-        //console.log(this.id);
-        this.classList.toggle('left-right')
-    })
-}
-
-
